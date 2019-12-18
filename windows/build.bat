36412ffb4ffb0fc74bb545ea824693c7936d3df7
bunjee <bunjee@omega.gg>
2019-12-18 17:04:03 +0100

    Create unix/build.sh (2)

---
+++ windows/build.bat
@@ -0,0 +1,13 @@
+set PATH=%cd%\MinGW\bin;%cd%\boost\tools\build\src\engine;%cd%\boost;%PATH%
+
+set BOOST_BUILD_PATH=%cd%\boost\tools\build\src
+
+set BOOST_ROOT=%cd%\boost
+
+cd boost\tools\build\src\engine
+
+call build.bat gcc
+
+cd ..\..\..\..\..\libtorrent
+
+b2.exe -j4 toolset=gcc cxxflags=-std=c++11 variant=release link=shared openssl-version=pre1.1
