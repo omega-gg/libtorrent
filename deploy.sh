249de37764ad116178d7b6ca37be917c25623819
bunjee <bunjee@omega.gg>
2023-09-15 18:40:35 +0200

    libtorrent 2.0.9

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -e
 # Settings
 #--------------------------------------------------------------------------------------------------
 
-libtorrent_versionA="2.0.8"
+libtorrent_versionA="2.0.9"
 libtorrent_versionB="2.0"
 
 Boost_version="1.78.0"
